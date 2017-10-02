ca9850c8c266754037cbd77c7595614136262004
= <sanbiv@gmail.com>
2017-10-02 17:07:43 +0200

    Using less-rails 3.0

--- lib/less/rails/bootstrap/engine.rb
+++ lib/less/rails/bootstrap/engine.rb
@@ -4,11 +4,12 @@ module Less
   module Rails
     module Bootstrap
       class Engine < ::Rails::Engine
-        
+
         initializer 'less-rails-bootstrap.setup', :after => 'less-rails.before.load_config_initializers', :group => :all do |app|
           app.config.less.paths << File.join(config.root, 'app', 'frameworks')
+          app.config.less.compress = true
         end
-        
+
       end
     end
   end
